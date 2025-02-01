a8d97bb34b410e82b921ed74579dddb707fb5203
nurazizah28 <sitinurazizahapr@gmail.com>
2025-02-01 17:23:39 +0700

    Tugas_Laravel2

---
+++ public/template/separate/form_genres.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h2>Find your taste in genres here</h2>
+    <div class="card">
+        <div class="card-header bg-primary text-white">Genre</div>
+        <div class="card-body">
+            <form>
+                <label>The movie genre you want to watch</label>
+                <input type="text" class="form-control" placeholder="Type here...">
+                <input type="checkbox"> For Real?
+                <button class="btn btn-primary mt-3">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    GenreController,
+    CastController,
+};
 
+Route::get('/iniagan', function () {
+    return view('agan');
+});
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/2', function () {
+    return view('index2');
+});
+
+// route buat nambahin genre
+// handler = create
+//PER GENRE-GENRE AN
+Route::controller(GenreController::class)->group(function () {
+    Route::get('/genre', 'index')->name('genre.index');
+    Route::get('/genre/create', 'create')->name('genre.create');
+    Route::post('/genre', 'store')->name('genre.store');
+    Route::get('/genre/{id}/edit', 'edit')->name('genre.edit');
+    Route::put('/genre/{id}', 'update')->name('genre.update');
+    Route::delete('/genre/{id}', 'destroy')->name('genre.destroy');
+});
+
+// CAST
+Route::controller(CastController::class)->group(function () {
+    Route::get('/cast','index')->name('cast.index');
+    Route::get('/cast/create','create')->name('cast.create');
+    Route::post('/cast','store')->name('cast.store');
+    Route::get('/cast/{id}/edit','edit')->name('cast.edit');
+    Route::put('/cast/{id}','update')->name('cast.update');
+    Route::delete('/cast/{id}','destroy')->name('cast.destroy');
 });
